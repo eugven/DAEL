6984eb0bb5e91e2c93a9e8c13935b721a9e22831
eugven <e.gorr@web.de>
2025-04-10 10:29:19 +0200

    Update html.js

--- src/@lekoarts/gatsby-theme-cara/html.js
+++ src/@lekoarts/gatsby-theme-cara/html.js
@@ -6,6 +6,9 @@ export default function HTML(props) {
       <head>
         {props.headComponents}
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#141821" />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
